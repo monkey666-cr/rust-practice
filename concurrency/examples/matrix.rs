9947d9001d466a3d0af43f6ea007fbe3f42c757c
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2024-12-30 21:10:25 +0800

    optimize(concurrency): 优化矩阵计算并行处理

--- concurrency/examples/matrix.rs
+++ concurrency/examples/matrix.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
-use concurrency::{multiply, Matrix};
+use concurrency::Matrix;
 
 fn main() -> Result<()> {
     let a = Matrix::new([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3);
     let b = Matrix::new([1, 2, 3], 3, 1);
 
-    let c = multiply(&a, &b).unwrap();
+    let c = a * b;
 
     println!("{:?}", c);
 
--- concurrency/src/martix.rs
+++ concurrency/src/martix.rs
@@ -1,9 +1,14 @@
 use core::fmt;
-use std::ops::{Add, AddAssign, Mul, MulAssign};
+use std::{
+    ops::{Add, AddAssign, Mul, MulAssign},
+    thread,
+};
 
 use anyhow::Result;
 
 use crate::{dot_product, Vector};
+
+const NUM_THREADS: usize = 4;
 
 pub struct Matrix<T> {
     data: Vec<T>,
@@ -56,15 +61,68 @@ where
     }
 }
 
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
+impl<T> Msg<T> {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Self { input, sender }
+    }
+}
+
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
 where
-    T: fmt::Debug + Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T> + MulAssign,
+    T: fmt::Debug
+        + Copy
+        + Default
+        + Add<Output = T>
+        + AddAssign
+        + Mul<Output = T>
+        + MulAssign
+        + Send
+        + 'static,
 {
     if a.col != b.row {
         panic!("Matrix dimensions are not compatible for multiplication");
     }
 
-    let mut data: Vec<T> = vec![T::default(); a.row * b.col];
+    // 定义异步线程的执行函数
+    let senders = (0..NUM_THREADS)
+        .map(|_| {
+            let (tx, rx) = std::sync::mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        value,
+                    }) {
+                        eprintln!("Failed to send result: {:?}", e);
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+
+    let matrix_len = a.row * b.col;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
+
     for i in 0..a.row {
         for j in 0..b.col {
             let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
@@ -74,8 +132,26 @@ where
                 .copied()
                 .collect::<Vec<_>>();
             let col = Vector::new(col_data);
-            data[i * b.col + j] += dot_product(row, col)?;
+            // 返回结果的索引
+            let idx = i * b.col + j;
+            // 输入消息
+            let input = MsgInput { idx, row, col };
+            // 一次性结果返回
+            let (tx, rx) = oneshot::channel();
+            // 构造消息
+            let msg = Msg::new(input, tx);
+            // 判断是否提交失败
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("Failed to send message: {:?}", e);
+            }
+            receivers.push(rx);
         }
+    }
+
+    // map/recude
+    for rx in receivers {
+        let output = rx.recv().unwrap();
+        data[output.idx] = output.value;
     }
 
     Ok(Matrix {
@@ -83,4 +159,23 @@ where
         row: a.row,
         col: b.col,
     })
+}
+
+impl<T> Mul for Matrix<T>
+where
+    T: fmt::Debug
+        + Copy
+        + Default
+        + Add<Output = T>
+        + AddAssign
+        + Mul<Output = T>
+        + MulAssign
+        + Send
+        + 'static,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        multiply(&self, &rhs).expect("Matrix multiplication failed")
+    }
 }
