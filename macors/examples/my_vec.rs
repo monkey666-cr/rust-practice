dadd2e888dbcd6f759a500da74953d3f731921c4
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-01-08 20:48:50 +0800

    优化宏声明

--- macors/examples/my_vec.rs
+++ macors/examples/my_vec.rs
@@ -1,9 +1,22 @@
-use macors::my_vec;
-
 fn main() {
-    let v = my_vec!(1, 2, 3);
+    let v: Vec<i32> = my_vec!(1, 2, 3);
     println!("{:?}", v);
 
     let v2 = vec![5; 10];
     println!("{:?}", v2);
+}
+
+#[macro_export]
+macro_rules! my_vec {
+    () => {
+        Vec::new()
+    };
+    ($elem:expr; $n:expr) => {
+        std::vec::from_elem($elem, $n)
+    };
+    ($($x:expr),+ $(,)?) => {
+        {
+            <[_]>::into_vec(Box::new([$($x),*]))
+        }
+    };
 }
